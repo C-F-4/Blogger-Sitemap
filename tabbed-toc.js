2ae320e5808384f31bc1c2e627e02bac62b7bbcc
cfx.404 <C-F-4@users.noreply.github.com>
2017-03-10 10:52:03 +0530

    Create tabbed-toc.js

---
+++ tabbed-toc.js
@@ -0,0 +1,38 @@
+<div id="tabbed-toc">
+<span class="loading">Loading, please wait for a moment...</span></div>
+<br />
+<script type="text/javascript">
+var tabbedTOC = {
+    blogUrl: "https://codersfabric.blogspot.in", // Enter your blog URL
+    containerId: "tabbed-toc", // Container ID
+    activeTab: 1, // The default active tab index (default: the first tab)
+    showDates: false, // `true` to show the post date
+    showSummaries: false, // `true` to show the posts summaries
+    numChars: 200, // Number of summary chars
+    showThumbnails: false, // `true` to show the posts thumbnails (Not recommended)
+    thumbSize: 40, // Default thumbnail size
+    noThumb: "", // A "no thumbnail" URL
+    monthNames: [ // Array of month names
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ],
+    newTabLink: true, // Open link in new window. `false` to open in same window
+    maxResults: 99999, // Maximum post results
+    preload: 0, // Load the feed after 0 seconds (option => time in milliseconds || "onload")
+    sortAlphabetically: true, // `false` to sort posts by published date
+    showNew: 7, // `false` to hide the "New!" mark in most recent posts or
+ //define how many recent posts are to be marked by changing the number
+    newText: " - <em style='color: white;padding: 1px 5px;border-radius: 20px;background-color: #F00;'>New!</em>" // HTML/CSS for
+//the "New!" text
+};
+</script>
